530376208b4cc908d0e55a80f97c57ee04410956
Alaan07 <rakeshofficial007@gmail.com>
2024-09-09 00:56:59 +0530

    almost landing page complete footer remaining and some design part still remaining

--- src/Components/Homepage.jsx
+++ src/Components/Homepage.jsx
@@ -1,5 +1,11 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import HopeImage from '../assets/image/hope2.png'
+import Itachi from '../assets/image/itachi.jpg'
 import Navbar from './Navbar.jsx'
+import { FaBookmark, FaHeart } from 'react-icons/fa'
+import VideoFile from '../assets/video/solo.mp4'
+
 
 function Homepage() {
   return (
@@ -18,6 +24,7 @@ function Homepage() {
              <div><p className='tillNowTableheading'>On HOPE : </p></div>
              <div className='tablediv'>
               <table className="tillNowTable">
+              <tbody>
                       <tr className='tablerowmain'>
                         <td className='tablecontents'>
                           <tr>
@@ -46,10 +53,198 @@ function Homepage() {
                           </tr>
                         </td>
                       </tr>
+                      </tbody>
               </table>
              </div>
-
           </div>
+
+          <div className='newProjects'>
+              <div className='featuredProjects'>
+                <div className='featuredHeading'>
+                  <h3 className='featuredTitle'> FEATURED PROJECTS</h3>
+                </div>
+                <div className='thumnaildiv'>
+                    <video className='previewVideo' controls muted autoPlay >
+                        <source src={VideoFile} type='video/mp4'/>
+                      error loading
+                    </video>
+                </div>
+                <div className='projectData'>
+                  <div className='dpblock'>
+                    <div className='projectdp'>
+                      <img className='profiledp' src={Itachi} alt="bla" />
+                    </div>
+                  </div>
+                  <div className='projectDetails'>
+                    <h3 className='projectTitle'> <FaHeart/> the best board game</h3>
+                    <p className='projectName'> BoardGames</p>
+                    <p  className='timeAndfunding'>clock icon, days left, dot 234%funded</p>
+                    <section className='projectDiscription'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum earum, nisi pariatur at possimus minima.</section>
+                    <div className='categoryAndlocation'>
+                        <button>Games</button>
+                        <button>karnataka, bangalore</button>
+                    </div>
+                  </div>
+                  <div className='bookmarkicon'>
+                    <FaBookmark/>
+                  </div>
+                </div>
+              </div>
+
+
+              <div className='recommendedProjects'>
+                <div className='recommendedHeading'>
+                  <h3 className='recommendedTitle'> RECOMMENDED PROJECTS</h3>
+                </div>
+                <div className='toprow'>
+                  <div className='card1 card'>
+
+                        <div className='thumnaildiv'>
+                          <video className='previewVideo' controls muted  >
+                              <source src={VideoFile} type='video/mp4'/>
+                            error loading
+                          </video>
+                      </div>
+                      <div className='projectData'>
+                        <div className='dpblock'>
+                          <div className='projectdp'>
+                            <img className='profiledp' src={Itachi} alt="bla" />
+                          </div>
+                        </div>
+                        <div className='projectDetails'>
+                          <h3 className='projectTitle'> <FaHeart/> the best board game</h3>
+                          <p className='projectName'> BoardGames</p>
+                          <p  className='timeAndfunding'>clock icon, days left, dot 234%funded</p>
+                          <section className='projectDiscription'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum earum, nisi pariatur at possimus minima.</section>
+                          <div className='categoryAndlocation'>
+                              <button>Games</button>
+                              <button>karnataka, bangalore</button>
+                          </div>
+                        </div>
+                        <div className='bookmarkicon'>
+                          <FaBookmark/>
+                        </div>
+                      </div>
+
+                  </div>
+
+                  <div className='card2 card'>
+
+                      <div className='thumnaildiv'>
+                          <video className='previewVideo' controls muted  >
+                              <source src={VideoFile} type='video/mp4'/>
+                            error loading
+                          </video>
+                      </div>
+                      <div className='projectData'>
+                        <div className='dpblock'>
+                          <div className='projectdp'>
+                            <img className='profiledp' src={Itachi} alt="bla" />
+                          </div>
+                        </div>
+                        <div className='projectDetails'>
+                          <h3 className='projectTitle'> <FaHeart/> the best board game</h3>
+                          <p className='projectName'> BoardGames</p>
+                          <p  className='timeAndfunding'>clock icon, days left, dot 234%funded</p>
+                          <section className='projectDiscription'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum earum, nisi pariatur at possimus minima.</section>
+                          <div className='categoryAndlocation'>
+                              <button>Games</button>
+                              <button>karnataka, bangalore</button>
+                          </div>
+                        </div>
+                        <div className='bookmarkicon'>
+                          <FaBookmark/>
+                        </div>
+                      </div>
+                    
+                  </div>
+                </div>
+                <div className='bottomrow'>
+                  <div className='card3 card'>
+
+                     <div className='thumnaildiv'>
+                          <video className='previewVideo' controls muted  >
+                              <source src={VideoFile} type='video/mp4'/>
+                            error loading
+                          </video>
+                      </div>
+                      <div className='projectData'>
+                        <div className='dpblock'>
+                          <div className='projectdp'>
+                            <img className='profiledp' src={Itachi} alt="bla" />
+                          </div>
+                        </div>
+                        <div className='projectDetails'>
+                          <h3 className='projectTitle'> <FaHeart/> the best board game</h3>
+                          <p className='projectName'> BoardGames</p>
+                          <p  className='timeAndfunding'>clock icon, days left, dot 234%funded</p>
+                          <section className='projectDiscription'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum earum, nisi pariatur at possimus minima.</section>
+                          <div className='categoryAndlocation'>
+                              <button>Games</button>
+                              <button>karnataka, bangalore</button>
+                          </div>
+                        </div>
+                        <div className='bookmarkicon'>
+                          <FaBookmark/>
+                        </div>
+                      </div>
+
+                  </div>
+                  <div className='card4 card'>
+
+                    <div className='thumnaildiv'>
+                          <video className='previewVideo' controls muted  >
+                              <source src={VideoFile} type='video/mp4'/>
+                            error loading
+                          </video>
+                      </div>
+                      <div className='projectData'>
+                        <div className='dpblock'>
+                          <div className='projectdp'>
+                            <img className='profiledp' src={Itachi} alt="bla" />
+                          </div>
+                        </div>
+                        <div className='projectDetails'>
+                          <h3 className='projectTitle'> <FaHeart/> the best board game</h3>
+                          <p className='projectName'> BoardGames</p>
+                          <p  className='timeAndfunding'>clock icon, days left, dot 234%funded</p>
+                          <section className='projectDiscription'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum earum, nisi pariatur at possimus minima.</section>
+                          <div className='categoryAndlocation'>
+                              <button>Games</button>
+                              <button>karnataka, bangalore</button>
+                          </div>
+                        </div>
+                        <div className='bookmarkicon'>
+                          <FaBookmark/>
+                        </div>
+                      </div>
+                    
+                  </div>
+                </div>
+
+                <div className='pages'>
+                <p>1 2 3 4</p>
+                </div>
+
+              </div>
+          </div>
+
+          <div className='newsANDevents'>
+                <div className='newsbanner'>
+                  <img className='hopebanner' src={HopeImage} alt="Hopebanner" />
+                </div>
+                <div className='newsupdates'>
+                  <h3 className='newsANDeventsheading'> Project Updates Blog</h3>
+                  <section className='newsANDeventsdetails'>
+                      A destination for company news, creator tips, project inspiration, and home to product updates sharing everything you need to know about what's happening at HOPE.
+                  </section>
+                  <Link className='newsReadmore' to="#">Read more</Link>
+                </div>
+          </div>
+
+
+
+
         </div>
       </div>
     </div>
